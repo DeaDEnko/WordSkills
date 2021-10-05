58bf350d852c85a7c931a0067418a4098f3ce6af
DeaDEnko <kill135gol@gmail.com>
2021-10-05 21:53:03 +0300

    Добавил БД

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+   
+        }
     }
 }
